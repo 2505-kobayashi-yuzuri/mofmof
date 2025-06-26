edac93b100f9d6005497b041a5bee8d7e3b4b4c4
kobayashi_yuzuri <kobayashi.yuzuri@alhinc.jp>
2025-06-26 16:29:46 +0900

    First commit

---
+++ src/main/java/com/example/mofmof/MofmofApplication.java
@@ -0,0 +1,13 @@
+package com.example.mofmof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MofmofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MofmofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mofmof/MofmofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mofmof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MofmofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
